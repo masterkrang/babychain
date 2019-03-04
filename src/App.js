7e09526669ffb92ee9b8867213991a019f377670
Kurt Braget <kurtbraget@gmail.com>
2019-03-03 17:24:59 -0800

    add core stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Input from '@material-ui/core/Input';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+
+
+const styles = {
+  app: 'margin 0px auto'
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Grid container spacing={24} alignContent='center'>
+          <Grid item xs={12}>
+            <TextField
+              xs={12}
+              lg={12}
+              id="standard-multiline-static"
+              label="Baby's Name"
+              placeholder="Satoshi Nakamoto Jr. III"
+              margin="normal"
+              fullWidth={true}
+            />
+          </Grid>
+          <Grid item xs={12} lg={12}>
+            <TextField
+              xs={12}
+              lg={12}
+              id="date"
+              label="Birthday"
+              type="date"
+              defaultValue="2017-05-24"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              fullWidth={true}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              xs={12}
+              lg={12}
+              id="standard-multiline-static"
+              label="Gender"
+              placeholder="Anything goes"
+              margin="normal"
+              fullWidth={true}
+            />
+          </Grid>
+          <Button variant="contained" color="primary">
+            Submit
+          </Button>
+        </Grid>
       </div>
     );
   }
