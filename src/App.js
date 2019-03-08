383c16afa59fbdc747acbc7df6a5d9c3ada17ede
Kurt Braget <kurtbraget@gmail.com>
2019-03-08 03:54:49 -0800

    add scatter request transfers

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 
+import ScatterJS from 'scatterjs-core';
+import ScatterEOS from 'scatterjs-plugin-eosjs2'
+import Eos from 'eosjs';
+
+ScatterJS.plugins( new ScatterEOS() );
+
 const PARENTS = "parents";
 const BIRTHDATE = "birthdate";
 const BABYNAME = "babyname";
 const GENDER = "gender";
 const ADDRESS = "address";
+const CHARLENGTH = 256;
 
-// const styles = {
-//   app: 'margin 0px auto'
-// }
+
+const network = {
+    blockchain:'eos',
+    host: 'proxy.eosnode.tools',
+    port:443,
+    protocol:'https',
+    chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
+}
 
 class App extends Component {
 
@@ -24,11 +36,14 @@ class App extends Component {
       gender: '',
       address: '',
       parents: '',
-      memo: ''
+      memo: '',
+      charsLeft: 252
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.submitClicked = this.submitClicked.bind(this);
+
+
   }
 
   render() {
@@ -98,6 +113,14 @@ class App extends Component {
               onChange={(evt) => this.handleChange(PARENTS, evt)}
             />
           </Grid>
+          <Grid item xs={12}>
+            <TextField
+              xs={12}
+              lg={12}
+              fullWidth={true}
+              value={this.state.charsLeft + " characters left."}>
+            </TextField>
+          </Grid>
           <Button
             onClick={this.submitClicked}
             variant="contained" color="primary">
@@ -129,15 +152,62 @@ class App extends Component {
       default:
         console.log("uh oh")
     }
+    this.setState({
+      memo: this.getMemo(),
+      charsLeft: CHARLENGTH - this.getMemo().length
+    })
+    //console.log(this.getMemo().length)
   }
 
   submitClicked() {
     console.log("submit clicked")
     console.log(this.state)
-    let s = this.state;
-    let memo = `${s.babyName}|${s.birthdate}|${s.gender}|${s.address}|${s.parents}`
+    let memo = this.getMemo();
 
     console.log("memo", memo)
+
+    ScatterJS.scatter.connect('babychain').then(connected => {
+
+        // If the user does not have Scatter or it is Locked or Closed this will return false;
+        if(!connected) return false;
+
+        const scatter = ScatterJS.scatter;
+
+        // Now we need to get an identity from the user.
+        // We're also going to require an account that is connected to the network we're using.
+        const requiredFields = { accounts:[network] };
+        scatter.getIdentity(requiredFields).then(() => {
+
+            // Always use the accounts you got back from Scatter. Never hardcode them even if you are prompting
+            // the user for their account name beforehand. They could still give you a different account.
+            const account = scatter.identity.accounts.find(x => x.blockchain === 'eos');
+
+            // You can pass in any additional options you want into the eosjs reference.
+            const eosOptions = { expireInSeconds:60 };
+
+            // Get a proxy reference to eosjs which you can use to sign transactions with a user's Scatter.
+            const eos = scatter.eos(network, Eos, eosOptions);
+
+            // Never assume the account's permission/authority. Always take it from the returned account.
+            //const transactionOptions = { authorization:[`${account.name}@${account.authority}`] };
+
+            const tokenDetails = {contract:'eosio.token', symbol:'EOS', memo:'test babychain memo', decimals:4};
+            scatter.requestTransfer(network, 'eosdividendz', '0.001', tokenDetails).then(result => {
+                console.log('result', result);
+            })
+
+        }).catch(error => {
+            // The user rejected this request, or doesn't have the appropriate requirements.
+            console.log("rejected request")
+            console.error(error);
+        });
+    });
+  }
+
+  getMemo(){
+    let s = this.state;
+    let memo = `${s.babyName}|${s.birthdate}|${s.gender}|${s.address}|${s.parents}`
+    return memo;
   }
 }
 
